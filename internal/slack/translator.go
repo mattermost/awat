642a63fde2a577459c2ddc9cc0abb14a18ea53da
Ian Whitlock <ian.whitlock@mattermost.com>
2021-03-09 11:29:23 -0600

    Refactor Translate into helper methods

--- internal/slack/translator.go
+++ internal/slack/translator.go
@@ -32,77 +32,102 @@ func (st *SlackTranslator) Translate(translation *model.Translation) error {
 		return err
 	}
 
-	logger := logrus.New().WithField("translation", translation.ID)
+	logger := logrus.New()
 
-	// fetch messages using .. the other tool,
+	inputArchiveName, err := st.fetchSlackArchive(logger, workdir, translation.Resource)
+	if err != nil {
+		return err
+	}
+
+	attachmentDirName := fmt.Sprintf("%s/attachments", workdir)
+	archiveWithFilesName, err := st.addFilesToSlackArchive(
+		logger,
+		workdir,
+		attachmentDirName,
+		inputArchiveName,
+	)
+
+	mbifName := workdir + "/" + translation.InstallationID + "_MBIF.jsonl"
+	logger.Infof("Transforming Slack archive for Translation %s to MBIF", translation.ID)
+	err = TransformSlack(
+		archiveWithFilesName,
+		mbifName,
+		translation.Team,
+		attachmentDirName,
+	)
+	if err != nil {
+		return errors.Wrap(err, "failed to transform Slack archive to MBIF")
+	}
+
+	_, err := st.createOutputZipfile(logger, attachmentDirName, mbifName, translation.ID)
+
+	logger.Infof("Finished translation %s", translation.ID)
+	return nil
+}
+
+func (st *SlackTranslator) fetchSlackArchive(logger logrus.FieldLogger, workdir, resource string) (string, error) {
+
 	sess := session.Must(session.NewSession())
 
 	downloader := s3manager.NewDownloader(sess)
 
 	inputArchive, err := os.Create(workdir + "/input.zip")
 	if err != nil {
-		return errors.Wrap(err, "failed to open temp file to download input archive to")
+		return "", errors.Wrap(err, "failed to open temp file to download input archive to")
 	}
 
 	nBytes, err := downloader.Download(inputArchive,
 		&s3.GetObjectInput{
 			Bucket: &st.bucket,
-			Key:    &translation.Resource,
+			Key:    &resource,
 		})
 
 	if err != nil {
-		return errors.Wrapf(err, "failed to download %s from bucket %s", translation.Resource, st.bucket)
+		return "", errors.Wrapf(err, "failed to download %s from bucket %s", resource, st.bucket)
 	}
 
 	err = inputArchive.Close()
 	if err != nil {
-		return errors.Wrap(err, "failed to close temporary file after writing incoming archive to it")
+		return "", errors.Wrap(err, "failed to close temporary file after writing incoming archive to it")
 	}
 
 	logger.Debugf("Successfully downloaded %d bytes from bucket %s key %s",
-		nBytes, st.bucket, translation.Resource)
+		nBytes, st.bucket, resource)
+
+	return inputArchive.Name(), nil
+}
+
+func (st *SlackTranslator) addFilesToSlackArchive(logger logrus.FieldLogger, workdir, attachmentDirName, inputArchiveName string) (string, error) {
+
+	logger.Infof("Downloading attached files to %s", attachmentDirName)
 
 	withFiles, err := os.Create(workdir + "/inputWithFiles.zip")
 	if err != nil {
-		return errors.Wrap(err, "failed to open temp file to convert input archive to")
+		return "", errors.Wrap(err, "failed to open temp file to convert input archive to")
 	}
 
-	err = FetchAttachedFiles(inputArchive.Name(), withFiles.Name())
+	err = FetchAttachedFiles(inputArchiveName, withFiles.Name())
 	if err != nil {
-		return errors.Wrap(err, "failed to fetch attached files")
+		return "", errors.Wrap(err, "failed to fetch attached files")
 	}
 
-	err = os.Remove(inputArchive.Name())
+	err = os.Remove(inputArchiveName)
 	if err != nil {
-		logger.Errorf("failed to remove file %s", inputArchive.Name())
+		logger.Errorf("failed to remove file %s", inputArchiveName)
 	}
 
-	attachmentDirName := fmt.Sprintf("%s/attachments", workdir)
 	err = os.Mkdir(attachmentDirName, 0700)
 	if err != nil {
-		return errors.Wrap(err, "failed to create attachments directory")
+		return "", errors.Wrap(err, "failed to create attachments directory")
 	}
 
-	mbifName := workdir + "/" + translation.InstallationID + "_MBIF.jsonl"
+	return withFiles.Name(), nil
+}
 
-	logger.Infof("Downloading attached files to %s", attachmentDirName)
-	err = TransformSlack(
-		withFiles.Name(),
-		mbifName,
-		translation.Team,
-		attachmentDirName)
-
+func (st *SlackTranslator) createOutputZipfile(logger logrus.FieldLogger, attachmentDirName, mbifName, translationID string) (string, error) {
+	output, err := os.Create(fmt.Sprintf("%s/%s.zip", st.workingDir, translationID))
 	if err != nil {
-		return err
-	}
-
-	if err != nil {
-		return errors.Wrap(err, "failed to transform Slack archive to MBIF")
-	}
-
-	output, err := os.Create(fmt.Sprintf("%s/%s.zip", st.workingDir, translation.ID))
-	if err != nil {
-		return err
+		return "", err
 	}
 	defer output.Close()
 
@@ -111,24 +136,24 @@ func (st *SlackTranslator) Translate(translation *model.Translation) error {
 
 	mbifInOutputZipfile, err := outputZipfile.Create("MBIF.jsonl")
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	mbifInputFile, err := os.Open(mbifName)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	_, err = io.Copy(mbifInOutputZipfile, mbifInputFile)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	mbifInputFile.Close()
 
 	attachmentFiles, err := ioutil.ReadDir(attachmentDirName)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	for _, attachment := range attachmentFiles {
@@ -155,6 +180,5 @@ func (st *SlackTranslator) Translate(translation *model.Translation) error {
 		}
 	}
 
-	logger.Infof("Finished translation %s", translation.ID)
-	return nil
+	return output.Name(), nil
 }
