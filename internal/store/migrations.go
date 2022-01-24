d2e9eff22276bc09f33d373dd9c8213a879c5b9c
Gabe Jackson <gabe@coffeepowered.co>
2022-01-24 16:38:19 -0500

    Migrate existing timestamps

--- internal/store/migrations.go
+++ internal/store/migrations.go
@@ -5,6 +5,8 @@
 package store
 
 import (
+	"fmt"
+
 	"github.com/blang/semver"
 )
 
@@ -77,6 +79,87 @@ var migrations = []migration{
 				);
 		`)
 			return err
+		},
+	},
+	{semver.MustParse("0.2.0"), semver.MustParse("0.3.0"),
+		func(e execer) error {
+			m := func(i int64) int64 {
+				if i == 0 {
+					return 0
+				}
+				return i / 1000
+			}
+
+			uploadRows, err := e.Query(`SELECT ID, CreateAt, CompleteAt FROM upload;`)
+			if err != nil {
+				return err
+			}
+			defer uploadRows.Close()
+
+			for uploadRows.Next() {
+				var id string
+				var createAt, completeAt int64
+				err := uploadRows.Scan(&id, &createAt, &completeAt)
+				if err != nil {
+					return err
+				}
+				_, err = e.Exec(fmt.Sprintf(`UPDATE upload SET CreateAt = %d, CompleteAt = %d WHERE ID = '%s';`, m(createAt), m(completeAt), id))
+				if err != nil {
+					return err
+				}
+			}
+			err = uploadRows.Err()
+			if err != nil {
+				return err
+			}
+
+			translationRows, err := e.Query(`SELECT ID, CreateAt, StartAt, CompleteAt FROM translation;`)
+			if err != nil {
+				return err
+			}
+			defer translationRows.Close()
+
+			for translationRows.Next() {
+				var id string
+				var createAt, startAt, completeAt int64
+				err := translationRows.Scan(&id, &createAt, &startAt, &completeAt)
+				if err != nil {
+					return err
+				}
+				_, err = e.Exec(fmt.Sprintf(`UPDATE translation SET CreateAt = %d, StartAt = %d, CompleteAt = %d WHERE ID = '%s';`, m(createAt), m(startAt), m(completeAt), id))
+				if err != nil {
+					return err
+				}
+			}
+			err = translationRows.Err()
+			if err != nil {
+				return err
+			}
+
+			importRows, err := e.Query(`SELECT ID, CreateAt, StartAt, CompleteAt FROM import;`)
+			if err != nil {
+				return err
+			}
+			defer importRows.Close()
+
+			for importRows.Next() {
+				var id string
+				var createAt, startAt, completeAt int64
+				err := importRows.Scan(&id, &createAt, &startAt, &completeAt)
+				if err != nil {
+					return err
+				}
+				_, err = e.Exec(fmt.Sprintf(`UPDATE import SET CreateAt = %d, StartAt = %d, CompleteAt = %d WHERE ID = '%s';`, m(createAt), m(startAt), m(completeAt), id))
+				if err != nil {
+					return err
+				}
+			}
+			err = importRows.Err()
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	},
 }
