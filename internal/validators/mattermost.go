3c2db73523c4ad9f1a78c66699036a5f19ad4866
Felipe Martin <me@fmartingr.com>
2023-01-26 10:13:35 +0100

    deps: updated mmctl to 7.7.1

--- internal/validators/mattermost.go
+++ internal/validators/mattermost.go
@@ -1,7 +1,6 @@
 package validators
 
 import (
-	mmModel "github.com/mattermost/mattermost-server/v6/model"
 	"github.com/mattermost/mmctl/v6/commands/importer"
 )
 
@@ -12,12 +11,7 @@ func (v *MattermostValidator) Validate(archiveName string) error {
 	mmctlValidator := importer.NewValidator(
 		archiveName,
 		false,
-		false,
 		true,
-		make(map[string]*mmModel.Team),
-		make(map[importer.ChannelTeam]*mmModel.Channel),
-		make(map[string]*mmModel.User),
-		make(map[string]*mmModel.User),
 	)
 	return mmctlValidator.Validate()
 }
