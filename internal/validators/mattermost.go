c35c42f3aa73cdfb173ef2cae1eaf0f5a1c1aac6
Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>
2024-05-21 00:13:27 -0400

    Update dependencies
    This update includes the newest Mattermost server packages as well
    as using the very latest mmctl and mmetl code to avoid conflicts.
    
    The signatures for the transformer and validator functions have
    changed as a result and may require tuning in the future.

--- internal/validators/mattermost.go
+++ internal/validators/mattermost.go
@@ -1,7 +1,8 @@
 package validators
 
 import (
-	"github.com/mattermost/mmctl/v6/commands/importer"
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/v8/cmd/mmctl/commands/importer"
 )
 
 // MattermostValidator is a type that provides validation functionality for Mattermost data archives.
@@ -11,11 +12,25 @@ type MattermostValidator struct {
 // Validate checks the validity of a Mattermost data archive.
 // It uses the mmctl tool's validation process, ensuring the archive is correctly formatted and structured.
 func (v *MattermostValidator) Validate(archiveName string) error {
+	// TODO: look into ways to populate existing data.
+
+	serverTeams := make(map[string]*model.Team)
+	serverChannels := make(map[importer.ChannelTeam]*model.Channel)
+	serverUsers := make(map[string]*model.User)
+	serverEmails := make(map[string]*model.User)
+
 	mmctlValidator := importer.NewValidator(
-		archiveName,
-		false,
-		true,
+		archiveName,    // input file
+		false,          // ignore attachments
+		true,           // create missing teams flag
+		true,           // check for server duplicates
+		serverTeams,    // map of existing teams
+		serverChannels, // map of existing channels
+		serverUsers,    // map of users by name
+		serverEmails,   // map of users by email
+		16383,          // max post size - taken from mmctl logic
 	)
+
 	return mmctlValidator.Validate()
 }
 
